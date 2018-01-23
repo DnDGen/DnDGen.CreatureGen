941616eb789761cfb313cb55e4a462f1f4aad6d8
Karl Speer <kspeer@globaledit.com>
2018-01-23 18:10:00 -0500

    got it stubbed

---
+++ CreatureGen.Tests.Unit/Selectors/Collections/CreatureDataSelectorTests.cs
@@ -0,0 +1,23 @@
+﻿using CreatureGen.Selectors.Collections;
+using NUnit.Framework;
+
+namespace CreatureGen.Tests.Unit.Selectors.Collections
+{
+    [TestFixture]
+    public class CreatureDataSelectorTests
+    {
+        private ICreatureDataSelector creatureDataSelector;
+
+        [SetUp]
+        public void Setup()
+        {
+            creatureDataSelector = new CreatureDataSelector();
+        }
+
+        [Test]
+        public void SelectCreatureData()
+        {
+            Assert.Fail("Not yet written");
+        }
+    }
+}
